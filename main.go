0b016726621415ccb46f33c01fc5b53acacada33
Fiddler46 <adhyaksh99@gmail.com>
2021-12-08 20:37:38 +0530

    Added creation of user endpoint and routing

--- main.go
+++ main.go
@@ -1,43 +1,51 @@
 package main
 
 import (
-	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 
-	
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"github.com/gorilla/mux"
 )
 
+// model for user endpoint
 type User struct {
-	Email       string `json:"email"`
-	AccessToken string `json:"token"`
+	Email string `json:"email"`
 }
 
-type Users struct {
-	Users []User
-}
+// fake db to temp store users
+var users []User
 
-func new() *Users {
-	return &Users{}
+// checks if json is empty or not
+func (u *User) IsEmpty() bool {
+	return u.Email == ""
 }
 
 func main() {
-	http.HandleFunc("/user", createUser).Methods(http.MethodPost)
-	log.Fatal(http.ListenAndServe(":8080", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/user", createUser).Methods("POST")
+	fmt.Println("Server running at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", r))
 
-	setup()
 }
 
+func createUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Body == nil {
+		json.NewEncoder(w).Encode("Must send data")
+	}
 
-
-func createUser(r *Users) (user *User) {
-	
-	r.Users = append(r.Users, user)
-	
+	var user User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if user.IsEmpty() {
+		json.NewEncoder(w).Encode("Invalid! Enter user email.")
+		return
+	}
+	users = append(users, user)
+	json.NewEncoder(w).Encode(user)
 
 }
