319d084ea7bddc64287c2e91e6b9de69bcd4c1d3
Fiddler46 <adhyaksh99@gmail.com>
2021-12-23 13:53:36 +0530

    Set mongodb connection to global client

--- main.go
+++ main.go
@@ -16,22 +16,23 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func setup() mongo.Database {
+var client mongo.Client
+
+func setup() {
 	clientOptions := options.Client().
 		ApplyURI("mongodb+srv://Fiddler46:Fiddler46@cluster0.um5qb.mongodb.net/cities-nighthack?retryWrites=true&w=majority")
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, clientOptions)
+	clientLocal, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = client.Ping(ctx, readpref.Primary())
+	err = clientLocal.Ping(ctx, readpref.Primary())
 	if err != nil {
 		log.Fatal(err)
 	}
-	//defer client.Disconnect(ctx)
-	DB := client.Database("cities-nighthack")
-	return *DB
+	fmt.Println("MongoDB started successfully!")
+	client = *clientLocal
 }
 
 // model for user endpoint
@@ -45,15 +46,8 @@ type City struct {
 	// CountryName string `bson:"country_name" json:"country"`
 }
 
-// fake db to temp store users
-var users []User
-
-// checks if json is empty or not
-func (u *User) IsEmpty() bool {
-	return u.Email == ""
-}
-
 func main() {
+	setup()
 	r := mux.NewRouter()
 	r.HandleFunc("/user", createUser).Methods("POST")
 	r.HandleFunc("/suggest", searchCity).Methods("GET")
@@ -64,55 +58,32 @@ func main() {
 }
 
 func createUser(w http.ResponseWriter, r *http.Request) {
-	DB := setup()
-	userCollection := DB.Collection("user")
-	// w.Header().Set("Content-Type", "application/json")
-	// if r.Body == nil {
-	// 	json.NewEncoder(w).Encode("Must send data")
-	// }
-
+	w.Header().Add("content-type", "application/json")
 	var user User
 	_ = json.NewDecoder(r.Body).Decode(&user)
+	userCollection := client.Database("cities-nighthack").Collection("user")
 	inserted, err := userCollection.InsertOne(context.Background(), user)
 	if err != nil {
 		log.Fatal(err)
 	}
-	json.NewEncoder(w).Encode(user)
+	json.NewEncoder(w).Encode(inserted)
 	fmt.Println("Inserted user into db: ", inserted.InsertedID)
-	// err := json.NewDecoder(r.Body).Decode(&user)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if user.IsEmpty() {
-	// 	json.NewEncoder(w).Encode("Invalid! Enter user email.")
-	// 	return
-	// }
-	// users = append(users, user)
-	// json.NewEncoder(w).Encode(user)
 
 }
 
 func searchCity(w http.ResponseWriter, r *http.Request) {
 	// ctx := context.Background()
 	w.Header().Set("Content-Type", "application/json")
-	DB := setup()
 	values := r.URL.Query()
 	city := values["city_name"]
-	cityCollection := DB.Collection("city")
+	cityCollection := client.Database("cities-nighthack").Collection("city")
 	fmt.Println(city)
 
 	if len(city) == 0 {
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"msg": "Search key required"}`))
+		w.Write([]byte(`{"Error": "City name cannot be empty!"}`))
 		return
 	}
-	// projection := bson.M{
-	// 	"all_names":    *city,
-	// 	"country_name": *city,
-	// }
-
-	// params := mux.Vars(r)
-	// city := params["city"]
 
 	filter := bson.D{
 		primitive.E{
@@ -137,7 +108,7 @@ func searchCity(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			log.Fatal(err)
 			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte(`{"msg": "There was an error. Try again later"}`))
+			w.Write([]byte(`{"Error": "Error recieved. Please try again"}`))
 			return
 		}
 		cityList = append(cityList, cities)
@@ -148,22 +119,5 @@ func searchCity(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	json.NewEncoder(w).Encode(cityList)
-	// if err = cursor.All(r.Context(), &cityList); err != nil {
-	// 	http.Error(w, err.Error(), http.StatusInternalServerError)
-	// }
 
-	// for _, cityList := range cityList {
-	// 	fmt.Println(cityList["all_names"])
-	// 	fmt.Println(cityList["country_name"])
-	// }
-
-	// defer cursor.Close(ctx)
-	// for cursor.Next(ctx) {
-	// 	var cityList bson.M
-	// 	if err = cursor.Decode(&cityList); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	fmt.Println(cityList["all_names"])
-	// 	fmt.Println(cityList["country_name"])
-	// }
 }
